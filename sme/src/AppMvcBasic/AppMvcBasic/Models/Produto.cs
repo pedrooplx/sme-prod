67c78c867f515d499126d43f06f66de665d3d7ad
Pedro Henrique <pedrooplx@gmail.com>
2021-03-01 23:22:28 -0300

    definição de data annotations para entidades e criação de migração

--- sme/src/AppMvcBasic/AppMvcBasic/Models/Produto.cs
+++ sme/src/AppMvcBasic/AppMvcBasic/Models/Produto.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +11,25 @@ namespace AppMvcBasic.Models
     public class Produto : Entity
     {
         public Guid FornecedorId { get; set; }
+        
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres.", MinimumLength = 5)]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres.", MinimumLength = 10)]
         public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         public string Imagem { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [Column(TypeName = "DECIMAL(18, 2)")]
         public decimal Valor { get; set; }
+
         public DateTime DataCadastro { get; set; }
+
+        [DisplayName("Ativo?")]
         public bool Ativo { get; set; }
 
         //EF Relations
