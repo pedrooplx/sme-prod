c9e441057f324360319815b0670d17a99b8b7e46
Pedro Henrique <pedrooplx@gmail.com>
2021-03-01 23:58:58 -0300

    criação de views de fornecedores

--- sme/src/AppMvcBasic/AppMvcBasic/Startup.cs
+++ sme/src/AppMvcBasic/AppMvcBasic/Startup.cs
@@ -30,9 +30,11 @@ namespace AppMvcBasic
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews();
+
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddRazorPages();
         }
 
