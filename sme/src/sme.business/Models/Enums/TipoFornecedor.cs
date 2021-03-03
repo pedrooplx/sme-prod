c924a24598bdd8fab09bd53d3df225c44f80a0cc
Pedro Henrique <pedrooplx@gmail.com>
2021-03-02 23:32:11 -0300

    criação de entidades da camada de business

---
+++ sme/src/sme.business/Models/Enums/TipoFornecedor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sme.business.Models.Enums
+{
+    public enum TipoFornecedor
+    {
+        PessoaFisica = 1,
+        PessoaJuridica
+    }
+}
