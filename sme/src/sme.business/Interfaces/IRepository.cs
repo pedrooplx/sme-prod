7813ffe01319faf8176a22718136d798e0dbaa7b
Pedro Henrique <pedrooplx@gmail.com>
2021-03-03 23:10:41 -0300

    criação de repositório genérico

---
+++ sme/src/sme.business/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using sme.business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sme.business.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        Task<IList<TEntity>> ObterTodos();
+        Task<TEntity> ObterPorId(Guid id);
+        Task Adicionar(TEntity entity);
+        Task Atualizar(TEntity entity);
+        Task Remover(Guid id);
+        Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate);
+        Task<int> SaveChanges();
+    }
+}
