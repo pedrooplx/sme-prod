422d0ceb8332ead5bfb99ea4f4002ce25c5c0e48
Pedro Henrique <pedrooplx@gmail.com>
2021-03-11 22:10:58 -0300

    scaffolding da controller de fornecedores

--- sme/src/sme.app/Data/ApplicationDbContext.cs
+++ sme/src/sme.app/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using sme.app.ViewModels;
 
 namespace sme.app.Data
 {
@@ -12,5 +13,6 @@ namespace sme.app.Data
             : base(options)
         {
         }
+
     }
 }
--- sme/src/sme.app/ViewModels/ProdutoViewModel.cs
+++ sme/src/sme.app/ViewModels/ProdutoViewModel.cs
@@ -14,6 +14,10 @@ namespace sme.app.ViewModels
         public Guid Id { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [DisplayName("Fornecedor")]
+        public Guid FornecedorId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres.", MinimumLength = 5)]
         public string Nome { get; set; }
 
