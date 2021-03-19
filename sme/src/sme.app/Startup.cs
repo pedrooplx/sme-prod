f2add6c88007227d284163a089eb09ee6063e90b
Pedro Henrique <pedrooplx@gmail.com>
2021-03-18 23:28:43 -0300

    adicionando globalização à aplicação

--- sme/src/sme.app/Startup.cs
+++ sme/src/sme.app/Startup.cs
@@ -82,6 +82,15 @@ namespace sme.app
             app.UseAuthentication();
             app.UseAuthorization();
 
+            var defaultCulture = new CultureInfo("pt-BR");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(defaultCulture),
+                SupportedCultures = new List<CultureInfo> { defaultCulture },
+                SupportedUICultures = new List<CultureInfo> { defaultCulture }
+            };
+            app.UseRequestLocalization(localizationOptions);
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
