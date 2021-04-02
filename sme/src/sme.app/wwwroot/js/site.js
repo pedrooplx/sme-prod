a4a8df6acdbe5df35142ced51e2bd26736e27596
Pedro Henrique <pedrooplx@gmail.com>
2021-04-01 22:34:09 -0300

    criação de modal para editar endereço do fornecedor

--- sme/src/sme.app/wwwroot/js/site.js
+++ sme/src/sme.app/wwwroot/js/site.js
@@ -1,41 +1,43 @@
-﻿function AjaxModal() {
+﻿function SetModal() {
+
     $(document).ready(function () {
         $(function () {
             $.ajaxSetup({ cache: false });
 
             $("a[data-modal]").on("click",
                 function (e) {
-                    debugger
-                    $("#myModalContent").load(this.href, function () {
-                        debugger
-                        //$("myModal").modal({ keyboard: true }, 'show');
-                        $("myModal").modal();
-                        //    bindForm(this);
-                    });
+                    $('#myModalContent').load(this.href,
+                        function () {
+                            $('#myModal').modal({
+                                keyboard: true
+                            },
+                                'show');
+                            bindForm(this);
+                        });
                     return false;
                 });
         });
+    });
+}
 
-        //function bindForm(dialog) {
-        //    debugger
-        //    $('form', dialog).submit(function () {
-        //        $.ajax({
-        //            url: this.action,
-        //            type: this.method,
-        //            data: $(this).serialize(),
-        //            success: function (result) {
-        //                debugger
-        //                if (result.success) {
-        //                    $('#myModal').modal('hide');
-        //                    $('#EnderecoTarget').load(result.url);
-        //                } else {
-        //                    $('#myModalContent').html(result);
-        //                    bindForm(dialog);
-        //                }
-        //            }
-        //        });
-        //        return false;
-        //    });
-        //}
+function bindForm(dialog) {
+    $('form', dialog).submit(function () {
+        $.ajax({
+            url: this.action,
+            type: this.method,
+            data: $(this).serialize(),
+            success: function (result) {
+                if (result.success) {
+                    $('#myModal').modal('hide');
+                    $('#EnderecoTarget').load(result.url); // Carrega o resultado HTML para a div demarcada
+                } else {
+                    $('#myModalContent').html(result);
+                    bindForm(dialog);
+                }
+            }
+        });
+
+        SetModal();
+        return false;
     });
 }
