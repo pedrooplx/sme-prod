1f5e1a91d09407e94fe1ce339bca8f613961eb1a
Pedro Henrique <pedrooplx@gmail.com>
2021-04-07 23:12:22 -0300

    exibição de elementos html de acordo com a claim (nível de acesso) atribuída ao perfil logado

---
+++ sme/src/sme.app/Extentions/EscondeElementoByClaimTagHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sme.app.Extentions
+{
+    [HtmlTargetElement("*", Attributes = "supress-by-claim-name")]
+    [HtmlTargetElement("*", Attributes = "supress-by-claim-value")]
+    public class EscondeElementoByClaimTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public EscondeElementoByClaimTagHelper(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        [HtmlAttributeName("supress-by-claim-name")]
+        public string IdentityClaimName { get; set; }
+
+        [HtmlAttributeName("supress-by-claim-value")]
+        public string IdentityClaimValue { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            if (output == null) throw new ArgumentNullException(nameof(output));
+
+            var temAcesso = CustomAuthorization.ValidarClaimsUsuario(_contextAccessor.HttpContext, IdentityClaimName, IdentityClaimValue);
+
+            if (temAcesso) return;
+
+            output.SuppressOutput();
+        }
+    }
+}
